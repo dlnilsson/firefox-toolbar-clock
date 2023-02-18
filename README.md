9b02d615ed24b06112ffb19321173c9f095533ef
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2023-02-18 16:32:51 +0100

    init commit
    2 firefox plugins, show Hour and minutes
    place them in the toolbar next to each other to see the time.
    
    load from file, temporary extensions
    
    about:debugging#/runtime/this-firefox

---
+++ README.md
@@ -0,0 +1,6 @@
+# firefox toolbar clock
+
+
+![](./example.png)
+
+2 extensions, that combined together, display a clock in the toolbar.
---
+++ hour/background.js
@@ -0,0 +1,13 @@
+function updateClock() {
+    const now = new Date();
+    const svg = `
+        <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32'>
+            <rect width='100%' height='100%' fill='transparent'/>
+            <text x='50%' y='50%' font-size='32' font-family='Arial' fill='white'
+                  text-anchor='middle' dominant-baseline='middle'>${String(now.getHours()).padStart(2, '0')}</text>
+        </svg>`;
+    browser.browserAction.setIcon({ path: `data:image/svg+xml;base64,${btoa(svg)}` });
+}
+updateClock();
+setInterval(updateClock, 60000);
+
