4df5a32a95803faa1e934b7c0f1e03ea84d69e77
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2025-02-19 10:30:15 +0100

    dynamic update interval on minute
    for more accurate update on minutes icon

--- minute/background.js
+++ minute/background.js
@@ -1,12 +1,19 @@
 function updateClock() {
-    const now = new Date();
-    const svg = `
-        <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32'>
-            <rect width='100%' height='100%' fill='transparent'/>
-            <text x='50%' y='50%' font-size='32' font-family='Arial' fill='white'
-                  text-anchor='middle' dominant-baseline='middle'>${String(now.getMinutes()).padStart(2, '0')}</text>
-        </svg>`;
-    browser.browserAction.setIcon({ path: `data:image/svg+xml;base64,${btoa(svg)}` });
+    browser.browserAction.setIcon({
+        path: `data:image/svg+xml;base64,${btoa(`
+            <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32'>
+                <rect width='100%' height='100%' fill='transparent'/>
+                <text x='50%' y='50%' font-size='32' font-family='Arial' fill='white'
+                      text-anchor='middle' dominant-baseline='middle'>
+                      ${String(new Date().getMinutes()).padStart(2, '0')}
+                </text>
+            </svg>`
+        )}`
+    });
 }
+
 updateClock();
-setInterval(updateClock, 60000);
+setTimeout(() => {
+    updateClock();
+    setInterval(updateClock, 60000);
+}, (60 - new Date().getSeconds()) * 1000);
